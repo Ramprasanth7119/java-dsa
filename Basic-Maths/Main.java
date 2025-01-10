896b9c839f61aec3ec923d0863e485f281ec2202
Ramprasanth7119 <ramprasanth2802@gmail.com>
2025-01-10 20:01:25 +0530

    basic maths added

--- Basic-Maths/Main.java
+++ Basic-Maths/Main.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static boolean isPrime(int n){
@@ -14,7 +19,30 @@ public class Main {
         if(cnt == 2) return true;
         return false;
     }
+
+    public static void factors(int n , List<Integer> arr){
+
+        for(int i = 1 ; i * i < n ; i++){
+            if(n % i == 0){
+                arr.add(i);
+                if((n/i) != i) arr.add(n/i);
+            }
+            
+        }
+    }
     public static void main(String[] args) {
         System.out.println(isPrime(1001));
+
+        List<Integer> ans = new ArrayList<>();
+
+        int n = 10;
+
+        factors(n, ans);
+
+        Collections.sort(ans);
+
+        for(int i : ans){
+            System.out.print(i + " ");
+        }
     }
 }
