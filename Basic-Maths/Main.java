ba31763245614d51e6af71c6341a4bb4af0cbce9
Ramprasanth7119 <ramprasanth2802@gmail.com>
2025-01-13 07:53:42 +0530

    gcd or hcf added

--- Basic-Maths/Main.java
+++ Basic-Maths/Main.java
@@ -87,6 +87,32 @@ public class Main {
             
         }
     }
+
+    public static int gcdI(int n1 , int n2){
+
+        int gcd = 1;
+        for(int i=1;i<=n1;i++){
+            if(n1 % i == 0 && n2 % i == 0){
+                gcd =  i;
+            }
+        }
+
+        return gcd;
+    }
+
+    public static int gcdII(int n1 , int n2){
+        
+        while(n1 > 0 && n2 > 0){
+
+            if(n1 > n2) n1 = n1 % n2;
+            else n2 = n2 % n1;
+
+        }
+        if(n1 == 0) return n2;
+        return n1;
+    }
+
+
     public static void main(String[] args) {
 
         System.out.println(numberCount(7659));  // O(log (N))
@@ -113,5 +139,11 @@ public class Main {
         for(int i : ans){
             System.out.print(i + " ");
         }
+
+        System.out.println();
+
+        System.out.println(gcdI(12,24)); // O(Min(n1,n2))
+
+        System.out.println(gcdII(20,40)); // O(log pi (min(n1,n2)))
     }
 }
