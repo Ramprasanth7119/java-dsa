86626ec4110e53086949f9a54520fb41987f9416
Ramprasanth7119 <ramprasanth2802@gmail.com>
2025-01-30 00:10:16 +0530

    NumberofCharactersWith3SubString added

---
+++ Sliding-Window/NumberofCharactersWith3SubString.java
@@ -0,0 +1,33 @@
+
+
+public class NumberofCharactersWith3SubString {
+
+    public static int numberofCharactersWith3SubStringI(String s){
+
+        int n = s.length();
+
+        int cnt = 0;
+
+        for(int i = 0 ; i < n ; i++){
+
+            int hash[] = new int[3];
+
+            for(int j = i ; j < n ; j++){
+
+                hash[s.charAt(j) - 'a']++;
+
+                if(hash[0] + hash[1] + hash[2] == 3){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+   
+    public static void main(String[] args) {
+        
+        String str = "bbacba";
+
+        System.out.println(numberofCharactersWith3SubStringI(str));
+    }
+}
