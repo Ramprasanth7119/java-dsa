1875161c2b8732ee8f19bd5a14c5d024be4460d0
Ramprasanth7119 <ramprasanth2802@gmail.com>
2025-01-31 21:54:17 +0530

    updated

--- Sliding-Window/NumberofCharactersWith3SubString.java
+++ Sliding-Window/NumberofCharactersWith3SubString.java
@@ -11,7 +11,7 @@ public class NumberofCharactersWith3SubString {
         for(int i = 0 ; i < n ; i++){
 
             int hash[] = new int[3];
-
+ 
             for(int j = i ; j < n ; j++){
 
                 hash[s.charAt(j) - 'a']++;
